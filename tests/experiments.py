a0d6554f1224a7937a8a74713a9a3d10fbd1b581
Sergio Leiva <sergio.leiva@ing.uchile.cl>
2017-11-19 15:06:53 -0300

    Added testing from files. Removed sequential implementataion testing, as its tested on C++ now

--- tests/experiments.py
+++ tests/experiments.py
@@ -4,8 +4,6 @@ from datetime import datetime
 from src.cuda_gol import CudaGameOfLife
 from src.data_manager import DataManager
 from src.opencl_gol import OpenCLGameOfLife
-from src.sequential_gol import SequentialGameOfLife
-
 
 def basicTest(width: int, height: int, prob: float, iterations: int):
     test_file = 'test.out'
@@ -18,20 +16,12 @@ def basicTest(width: int, height: int, prob: float, iterations: int):
 
     cudaGol_small = CudaGameOfLife(test_file, program_file=str(cuda_file))
     openClGol_small = OpenCLGameOfLife(test_file, program_file=str(opencl_file))
-    sequential_small = SequentialGameOfLife(test_file)
-
-    time_start = datetime.now()
-    #sequential_small.iterate(iterations)
-    time_end = datetime.now()
-    diff = str((time_end - time_start).total_seconds())
-    print("Tiempo secuencial: " + diff)
 
     time_start = datetime.now()
     cudaGol_small.iterate(iterations)
     time_end = datetime.now()
     diff = str((time_end - time_start).total_seconds())
     print("Tiempo cuda: " + diff)
-    #del cudaGol_small
 
     time_start = datetime.now()
     openClGol_small.iterate(iterations)
@@ -39,5 +29,40 @@ def basicTest(width: int, height: int, prob: float, iterations: int):
     diff = str((time_end - time_start).total_seconds())
     print("Tiempo opencl: " + diff)
 
+
+def test_from_data():
+    for i in range(11):
+        for j in range(10):
+            curr = 2 ** (i+1)
+            currstr = str(curr)
+
+            file = currstr + "x" + currstr
+            src_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'src')
+            cuda_file = os.path.join(src_path, 'cuda_gol_kernel.cu')
+            opencl_file = os.path.join(src_path, 'opencl_gol_kernel.cl')
+
+            cudaGol_small = CudaGameOfLife(file, program_file=str(cuda_file))
+            openClGol_small = OpenCLGameOfLife(file, program_file=str(opencl_file))
+
+            iterations = 1000
+
+            out_cuda = open("out_cuda.csv", "a")
+            out_opencl = open("out_opencl.csv", "a")
+
+            time_start = datetime.now()
+            cudaGol_small.iterate(iterations)
+            time_end = datetime.now()
+            diff = str((time_end - time_start).total_seconds())
+            out_cuda.write(currstr + ", " + diff + "\n")
+            print("n:" + currstr + "\nTiempo cuda: " + diff)
+
+            time_start = datetime.now()
+            openClGol_small.iterate(iterations)
+            time_end = datetime.now()
+            diff = str((time_end - time_start).total_seconds())
+            out_opencl.write(currstr + ", " + diff + "\n")
+            print("Tiempo opencl: " + diff + "\niteraciones:" + currstr)
+
+
 if __name__ == "__main__":
-    basicTest(2000, 2000, 0.5, 100)
+    test_from_data()
